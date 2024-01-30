e447ad04b7d5098b7e10052f19aac80a67cf17e6
pedro-marin-sanchis <pedro09pm.eduacion@gmail.com>
2024-01-30 16:48:09 +0100

    Initial commit;

---
+++ src/main/java/com/uguinformatica/Bluemoon_AdminWeb/BluemoonAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.uguinformatica.Bluemoon_AdminWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluemoonAdminWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluemoonAdminWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uguinformatica/Bluemoon_AdminWeb/BluemoonAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uguinformatica.Bluemoon_AdminWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluemoonAdminWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
